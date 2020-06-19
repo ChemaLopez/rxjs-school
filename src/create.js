3a647fd320f573d21d687085f5cee2e01c8d787b
ChemaLopez <joselo01@ucm.es>
2020-06-19 11:45:13 +0200

    End dev04

--- src/create.js
+++ src/create.js
@@ -1,7 +1,12 @@
 import { displayLog } from './utils';
-
+import {of, range} from 'rxjs'
 export default () => {
     /** start coding */
+    const source = of(1,2,3,4,5,{'pipo':'Hello World'}, function(){displayLog('Pipo')})
+
+    const source2= range(3,10);
+
+    const subscription = source2.subscribe(data => displayLog(data));
 
 
 
