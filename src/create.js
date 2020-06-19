8f9296e76c02b17aa3d9869aed855c5c5ea7cf97
ChemaLopez <joselo01@ucm.es>
2020-06-19 12:36:13 +0200

    Update create.js

--- src/create.js
+++ src/create.js
@@ -1,9 +1,15 @@
 import { displayLog } from './utils';
-
+import {interval, timer} from 'rxjs'
 export default () => {
     /** start coding */
 
+    const source = interval(500);
+    
 
+    const subscription = source.subscribe(data => displayLog(data))
+    const subscription2 = source.subscribe(data => displayLog(data))
 
+    timer(3000).subscribe(()=>subscription.unsubscribe())
+    setTimeout(()=>{subscription2.unsubscribe()},3000)
     /** end coding */
 }
