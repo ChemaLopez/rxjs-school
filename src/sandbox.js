960a7833c0f9a6dc1edeb0219bd29aa74a9ef915
ChemaLopez <joselo01@ucm.es>
2020-06-23 15:35:55 +0200

    End dev10

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,6 +1,6 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, takeWhile, last,takeLast,tap } from 'rxjs/operators';
+import { map, takeWhile, last,takeLast,skip, tap } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
@@ -10,10 +10,13 @@ export default () => {
             Math.floor(val.offsetX/50), 
             Math.floor(val.offsetY/50)
         ]),
-        takeWhile( ([col, row]) => col > 3 ),
-        tap(val =>console.log(`valid in takeWhile: [${val}]`) ),
-        takeLast(3)
+        //takeWhile( ([col, row]) => col > 3 ),
+        //tap(val =>console.log(`valid in takeWhile: [${val}]`) ),
+        //takeLast(3)
         //last()
+        tap(val =>console.log(`cell: [${val}]`) ),
+        skip(4)
+
     );
 
     const subscription = click$.subscribe(data => displayLog(data));
